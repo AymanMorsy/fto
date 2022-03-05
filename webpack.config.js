c88d02167d74f20ae729b25a622c0ae52c2c3c66
AymanMorsy <aymanmorsy.eg@gmail.com>
2022-03-05 07:24:49 +0200

    resolve wtaching html changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
+// const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -8,17 +9,30 @@ module.exports = {
     filename: "bundel.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [{ test: /\.css/, use: ["style-loader", "css-loader"] }],
+  },
   plugins: [
     new htmlWebpackPlugin({
+      cache: false,
       filename: "index.html",
       template: path.resolve(__dirname, "src", "index.html"),
     }),
+
+    // new BrowserSyncPlugin(
+    //   {
+    //     // browse to http://localhost:3000/ during development,
+    //     // ./public directory is being served
+    //     host: "localhost",
+    //     port: 3000,
+    //     files: ["./dist/*.html"],
+    //     server: { baseDir: ["dist"] },
+    //
+    //   }),
   ],
   devServer: {
-    port: 8080,
     open: true,
-    compress: true,
+    watchFiles: ["src/*.html"],
     hot: true,
-    liveReload: true,
   },
 };
